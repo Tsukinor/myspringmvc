02750d55d18557f7e7e474ef9676309f48d28187
Jeffrey <894860131@qq.com>
2024-04-23 22:39:03 +0800

    创建项目，完成初始配置

---
+++ src/main/java/com/jeffrey/springmvc/servlet/DispatcherServletP.java
@@ -0,0 +1,33 @@
+package com.jeffrey.springmvc.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @program: myspringmvc
+ * @author: Jeffrey
+ * @create: 2024-04-23 22:20
+ * @description:
+ * 1.DispatcherServletP充当前端控制器（原生DispatcherServlet）
+ * 2.本质是个servlet，继承httpServlet
+ *
+ **/
+
+
+public class DispatcherServletP extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req,resp);
+        System.out.println("DispatcherServletP------doGet---被调用");
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("DispatcherServletP-----doPost---被调用");
+    }
+}
